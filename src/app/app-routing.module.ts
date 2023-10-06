d767ea6b4bae083435e766813fbb473f752bcd57
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-10-06 17:07:48 +0200

    add auth0 support

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', component: LandingPageComponent }
+  { path: '', component: LandingPageComponent },
+  { path: 'dashboard', component: DashboardComponent }
 ];
 
 @NgModule({
