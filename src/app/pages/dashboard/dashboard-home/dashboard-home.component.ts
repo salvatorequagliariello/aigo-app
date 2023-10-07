e6b3b1d1aad64cce64ff127b3bad1489ab361ff2
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-10-07 19:41:36 +0200

    add dashboard module and dashboard-home component

---
+++ src/app/pages/dashboard/dashboard-home/dashboard-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aigo-dashboard-home',
+  templateUrl: './dashboard-home.component.html',
+  styleUrls: ['./dashboard-home.component.css']
+})
+export class DashboardHomeComponent {
+
+}
