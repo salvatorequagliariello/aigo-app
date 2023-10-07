e763c233f169fcf11a218bcb3f52de0c9a5aab4f
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-10-07 19:57:37 +0200

    add aichat component

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardHomeComponent } from './dashboard-home/dashboard-home.component';
+import { AiChatComponent } from './ai-chat/ai-chat.component';
 
 
 
 @NgModule({
   declarations: [
-    DashboardHomeComponent
+    DashboardHomeComponent,
+    AiChatComponent
   ],
   imports: [
     CommonModule
