cb3255aab22ef35f4fa79a8a931cc41f7af0813a
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-10-20 18:52:18 +0200

    fix codeAi styling

--- src/app/pages/dashboard/code-ai/code-ai.component.ts
+++ src/app/pages/dashboard/code-ai/code-ai.component.ts
@@ -2,31 +2,52 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { CodeAiService } from '../services/code-ai.service';
 import { ChatCompletionMessage } from 'openai/resources/chat';
-import { ResponseObj } from 'src/app/models/interfaces';
+import { IconObj, ResponseObj } from 'src/app/models/interfaces';
+import { ManageUserTokensService } from '../services/manage-user-tokens.service';
 
 @Component({
   selector: 'aigo-code-ai',
   templateUrl: './code-ai.component.html',
-  styleUrls: ['./code-ai.component.css']
+  styleUrls: ['./code-ai.component.scss']
 })
 export class CodeAiComponent {
   chatForm!: FormGroup
-  
-  constructor( private codeAi: CodeAiService ) {}
+  chat: ChatCompletionMessage[] = this.codeAi.messages
+  response: ResponseObj =  this.codeAi.response;
+  apiError: boolean = this.response.errorMessage ? true : false;
+  isTokenAlertOpen: boolean = false;
+
+  constructor( private codeAi: CodeAiService, private userTk: ManageUserTokensService ) {}
   
   ngOnInit(): void {
+    this.userTk.checkUser();
     this.chatForm = new FormGroup({
       chatInput: new FormControl('', Validators.required)
     }) 
   }
   
-  chat: ChatCompletionMessage[] = this.codeAi.messages
-  response: ResponseObj =  this.codeAi.response;
-  
   async onSubmit() {
-    if (this.chatForm.valid) {
+    if (this.chatForm.valid && this.userTk.user.tokens >= 1) {
       const chatResponse = await this.codeAi.getCodeGeneration(this.chatForm.value.chatInput, this.chat, this.response);
+      this.userTk.updateUserTokens(-1);
       this.chatForm.reset(this.chatForm);
+    } else if (this.userTk.user.tokens <= 0) {
+      this.isTokenAlertOpen = true;
+    } else {
+      return;
     }
   }
+  
+  closeTokensAlert(isOpen: boolean) {
+    this.isTokenAlertOpen = isOpen;
+  }
+
+  closeApiErrorAlert(isOpen: boolean) {
+    this.apiError = isOpen;
+  }
+
+  codeIcon: IconObj = {
+    pathOne: "M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z",
+    pathTwo: "M6.854 4.646a.5.5 0 0 1 0 .708L4.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0zm2.292 0a.5.5 0 0 0 0 .708L11.793 8l-2.647 2.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708 0z"
+  };
 }
--- src/app/pages/dashboard/image-ai/image-ai.component.ts
+++ src/app/pages/dashboard/image-ai/image-ai.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { AmountObj, ResponseObj } from 'src/app/models/interfaces';
+import { AmountObj, IconObj, ResponseObj } from 'src/app/models/interfaces';
 import { ImageAiService } from '../services/image-ai.service';
 import { Image } from 'openai/resources';
 
@@ -11,6 +11,10 @@ import { Image } from 'openai/resources';
 })
 export class ImageAiComponent {
   chatForm!: FormGroup
+  images: Image[] = this.imageAi.images
+  response: ResponseObj =  this.imageAi.response;
+  amountOptions: AmountObj[] = this.imageAi.amountOptions;
+  resolutionOptions: AmountObj[] = this.imageAi.resolutionOptions;
   
   constructor( private imageAi: ImageAiService ) {}
 
@@ -22,15 +26,14 @@ export class ImageAiComponent {
     }) 
   }
   
-  images: Image[] = this.imageAi.images
-  response: ResponseObj =  this.imageAi.response;
-  amountOptions: AmountObj[] = this.imageAi.amountOptions;
-  resolutionOptions: AmountObj[] = this.imageAi.resolutionOptions;
-  
   async onSubmit() {
     if (this.chatForm.valid) {
       const chatResponse = await this.imageAi.getImageGeneration(this.chatForm.value.prompt, this.images, this.response, this.chatForm.value.resolution, this.chatForm.value.amount);
       this.chatForm.reset(this.chatForm);
     }
+  }
+
+  imageIcon: IconObj = {
+    pathOne: "M8 16A8 8 0 1 1 8 0a8 8 0 0 1 0 16ZM1 8a7 7 0 0 0 7 7 3.5 3.5 0 1 0 0-7 3.5 3.5 0 1 1 0-7 7 7 0 0 0-7 7Z"
   }
 }
